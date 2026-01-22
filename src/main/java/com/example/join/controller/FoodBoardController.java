2ca1208869c5e926dcdcdcee129fe694fbddf558
devhyunju <246410195+devhyunju@users.noreply.github.com>
2026-01-22 18:20:10 +0900

    상세페이지, 수정페이지

--- src/main/java/com/example/join/controller/FoodBoardController.java
+++ src/main/java/com/example/join/controller/FoodBoardController.java
@@ -3,6 +3,7 @@ import com.example.join.service.FoodBoardService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.join.entity.FoodBoard;
@@ -34,4 +35,33 @@ public class FoodBoardController {
     	foodBoardService.saveFood(foodBoard);
     	return "redirect:/board";
     }
+    
+    //게시글 상세 페이지 
+    @GetMapping("/board/view/{id}")
+    public String viewBoard(@PathVariable Long id, Model model) {
+    	model.addAttribute("board", foodBoardService.findById(id));
+    	return "foodboard-view";
+    }
+    
+    //게시글 수정 페이지 
+    @GetMapping("/board/edit/{id}")
+    public String editForm(@PathVariable Long id, Model model) {
+    model.addAttribute("board", foodBoardService.findById(id));
+    return "foodboard-edit";
+    }
+    
+    //게시글 수정 처리 
+    @PostMapping("/board/edit/{id}")
+    public String updateBoard(@PathVariable Long id, FoodBoard foodBoard) {
+    	foodBoardService.updateBoard(id, foodBoard);
+    	 return "redirect:/board/view/" + id;
+    }
+    
+    //게시글 삭제 처리 
+    @PostMapping("/board/delete/{id}")
+    public String deleteBoard(@PathVariable Long id) {
+    	foodBoardService.deleteBoard(id);
+    	return "redirect:/board";
+    }
+   
 }
--- src/main/java/com/example/join/service/FoodBoardService.java
+++ src/main/java/com/example/join/service/FoodBoardService.java
@@ -20,4 +20,24 @@ public class FoodBoardService {
     public void saveFood(FoodBoard foodBoard) {
         foodboardRepository.save(foodBoard);
     }
+ // ID로 게시글 찾기
+    public FoodBoard findById(Long id) {
+    	return foodboardRepository.findById(id)
+    			.orElseThrow(() -> new RuntimeException("게시글을 찾을 수 없습니다."));
+    }
+ // 게시글 수정  
+    public void updateBoard(Long id, FoodBoard updatedBoard) {
+    	FoodBoard board = findById(id);
+    	board.setTitle(updatedBoard.getTitle());
+        board.setRegion(updatedBoard.getRegion());
+        board.setPrefecture(updatedBoard.getPrefecture());
+        board.setRating(updatedBoard.getRating());
+        board.setContent(updatedBoard.getContent());
+        board.setImageUrl(updatedBoard.getImageUrl());
+        foodboardRepository.save(board);
+    }
+ // 게시글 삭제
+    public void deleteBoard(Long id) {
+        foodboardRepository.deleteById(id);
+    }
 }
